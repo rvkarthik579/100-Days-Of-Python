fbc0ff601359c7881d60582cab25a36649897975
rvkarthik579 <rvkarthik579@gmail.com>
2025-12-08 21:25:14 +0530

    Day 1 Complete

---
+++ Day-001-Band-Name/main.py
@@ -0,0 +1,4 @@
+print("welcome to band name generator")
+city=input("which city did you grew in?\n")
+pet_name=input("what is the name of your pet?\n")
+print("your band name could be: "+city+ ""+pet_name)
